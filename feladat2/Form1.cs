0f3031a077b3bc91f175ed8dbf4700c475b4a256
mlilla05 <lilla.mogyorosi05@gmail.com>
2024-03-07 11:19:44 +0100

    Add project files.

---
+++ feladat2/Form1.cs
@@ -0,0 +1,10 @@
+namespace feladat2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ software_zh1/Form1.cs
@@ -0,0 +1,10 @@
+namespace software_zh1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
